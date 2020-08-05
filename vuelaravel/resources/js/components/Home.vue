fa1f276cf91a00af89c8b1e554ecad6c6e461603
zapbuild-ankit <fbg.ankit@gmail.com>
2020-08-05 11:35:33 +0530

    vuelaravel auth

---
+++ vuelaravel/resources/js/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <div class="card card-default">
+            <div class="card-header">Bienvenue</div>
+            <div class="card-body">
+                <p>
+                    Hello there 
+                </p>
+                <p>
+                    How are you??
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
